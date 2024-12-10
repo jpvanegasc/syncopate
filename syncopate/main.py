c74e60ebc28b63b115e1b5763a0098b72bc499e8
Juan Vanegas <juan.vanegas@loka.com>
2024-12-09 21:36:35 -0500

    :sparkles: Custom implementation of `asyncio.start_server`

--- syncopate/main.py
+++ syncopate/main.py
@@ -1,4 +1,27 @@
 import asyncio
+import socket
+
+
+async def custom_start_server(client_handler, host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((host, port))
+    server_socket.listen()
+    server_socket.setblocking(False)
+
+    loop = asyncio.get_running_loop()
+    print(f"Server listening on {host}:{port}")
+
+    async def accept_connections():
+        while True:
+            client_socket, addr = await loop.sock_accept(server_socket)
+            print(f"Connection accepted from {addr}")
+
+            reader, writer = await asyncio.open_connection(sock=client_socket)
+
+            loop.create_task(client_handler(reader, writer))
+
+    await accept_connections()
 
 
 async def handle_client(reader, writer):
@@ -18,10 +41,7 @@ async def handle_client(reader, writer):
 
 
 async def main():
-    server = await asyncio.start_server(handle_client, "127.0.0.1", 8888)
-    async with server:
-        print("Server started on 127.0.0.1:8888")
-        await server.serve_forever()
+    await custom_start_server(handle_client, "127.0.0.1", 8888)
 
 
 if __name__ == "__main__":
