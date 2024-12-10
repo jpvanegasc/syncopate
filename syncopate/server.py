06a075081786e71064849a1172cfaf7095173fb2
Juan Vanegas <juan.vanegas@loka.com>
2024-12-10 15:02:49 -0500

    :sparkles: Parse and return an HTTP request

--- syncopate/server.py
+++ syncopate/server.py
@@ -1,15 +1,30 @@
 import asyncio
 
 
+def parse_http_request(request_text):
+    lines = request_text.split("\r\n")
+    method, path, _ = lines[0].split(" ", 2)
+
+    headers = {}
+    for line in lines[1:]:
+        if not line.strip():
+            break
+        key, value = line.split(":", 1)
+        headers[key.strip()] = value.strip()
+    return method, path, headers
+
+
 async def handle_client(reader, writer):
     addr = writer.get_extra_info("peername")
     print(f"New connection from {addr}")
 
     try:
-        while data := await reader.readline():
-            print(f"Received: {data.decode().strip()} from {addr}")
-            writer.write(data)
-            await writer.drain()
+        data = await reader.readuntil(b"\r\n\r\n")
+        request_text = data.decode()
+        method, path, headers = parse_http_request(request_text)
+        print(f"Received {method} request for {path} from {addr}, {headers=!r}")
+        response_text = "HTTP/1.1 200 OK\r\n\r\n<h1>Hello, world!</h1>"
+        writer.write(response_text.encode())
     except asyncio.IncompleteReadError:
         print(f"Connection with {addr} closed")
     finally:
