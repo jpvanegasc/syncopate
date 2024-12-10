590b49d944f3b125bb5d773c8b5bfbd405ab2e50
Juan Vanegas <juan.vanegas@loka.com>
2024-12-10 15:40:07 -0500

    :recycle: Centralize logic in server class

--- syncopate/main.py
+++ syncopate/main.py
@@ -1,41 +1,10 @@
 import asyncio
 import logging
-import socket
 
-from syncopate.server import handle_client
+from syncopate.server import Server
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(levelname)s:%(name)s %(message)s",
-)
 logger = logging.getLogger("syncopate")
 
-
-async def custom_start_server(client_handler, host, port):
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((host, port))
-    server_socket.listen()
-    server_socket.setblocking(False)
-
-    loop = asyncio.get_running_loop()
-    logger.info("Server listening on %s:%s", host, port)
-
-    async def accept_connections():
-        while True:
-            client_socket, addr = await loop.sock_accept(server_socket)
-            logger.info("Connection accepted from %s", addr)
-
-            reader, writer = await asyncio.open_connection(sock=client_socket)
-
-            loop.create_task(client_handler(reader, writer))
-
-    await accept_connections()
-
-
-async def main():
-    await custom_start_server(handle_client, "127.0.0.1", 8888)
-
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    server = Server("localhost", 8888)
+    asyncio.run(server.serve())
--- syncopate/server.py
+++ syncopate/server.py
@@ -1,42 +1,75 @@
 import asyncio
 import logging
+import socket
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s:%(name)s: %(message)s",
+)
 logger = logging.getLogger(__name__)
 
 
-def parse_http_request(request_text):
-    lines = request_text.split("\r\n")
-    method, path, _ = lines[0].split(" ", 2)
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
-    headers = {}
-    for line in lines[1:]:
-        if not line.strip():
-            break
-        key, value = line.split(":", 1)
-        headers[key.strip()] = value.strip()
-    return method, path, headers
+    @staticmethod
+    def parse_http_request(request_text):
+        lines = request_text.split("\r\n")
+        method, path, _ = lines[0].split(" ", 2)
 
+        headers = {}
+        for line in lines[1:]:
+            if not line.strip():
+                break
+            key, value = line.split(":", 1)
+            headers[key.strip()] = value.strip()
+        return method, path, headers
 
-async def handle_client(reader, writer):
-    addr = writer.get_extra_info("peername")
-    logger.info("New connection from %s", addr)
+    async def handle_client(self, reader, writer):
+        addr = writer.get_extra_info("peername")
+        logger.info("New connection from %s", addr)
 
-    try:
-        data = await reader.readuntil(b"\r\n\r\n")
-        request_text = data.decode()
-        method, path, headers = parse_http_request(request_text)
-        logger.info(
-            "Received %s request for %s from %s, headers: %s",
-            method,
-            path,
-            addr,
-            headers,
-        )
-        response_text = "HTTP/1.1 200 OK\r\n\r\n<h1>Hello, world!</h1>"
-        writer.write(response_text.encode())
-    except asyncio.IncompleteReadError:
-        logger.info("Connection with %s closed", addr)
-    finally:
-        writer.close()
-        await writer.wait_closed()
+        try:
+            data = await reader.readuntil(b"\r\n\r\n")
+            request_text = data.decode()
+            method, path, headers = self.parse_http_request(request_text)
+            logger.info(
+                "Received %s request for %s from %s, headers: %s",
+                method,
+                path,
+                addr,
+                headers,
+            )
+            response_text = "HTTP/1.1 200 OK\r\n\r\n<h1>Hello, world!</h1>"
+            writer.write(response_text.encode())
+        except asyncio.IncompleteReadError:
+            logger.info("Connection with %s closed", addr)
+        finally:
+            writer.close()
+            await writer.wait_closed()
+
+    async def serve(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((self.host, self.port))
+        server_socket.listen()
+        server_socket.setblocking(False)
+
+        loop = asyncio.get_running_loop()
+        logger.info("Server listening on %s:%s", self.host, self.port)
+
+        async def accept_connections():
+            while True:
+                client_socket, addr = await loop.sock_accept(server_socket)
+                logger.info("Connection accepted from %s", addr)
+
+                reader, writer = await asyncio.open_connection(sock=client_socket)
+
+                loop.create_task(self.handle_client(reader, writer))
+
+        await accept_connections()
+
+    def run(self, host, port):
+        return asyncio.run(self.serve())
