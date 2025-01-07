14027d6b90198467e63bbb88ae5398fc5efad780
Juan Vanegas <juan.vanegas@loka.com>
2025-01-07 18:46:19 -0500

    :sparkles: Hello world example using routes

--- examples/full.py
+++ examples/full.py
@@ -5,6 +5,14 @@ from syncopate.framework import Syncopate
 
 logger = logging.getLogger(__name__)
 
+
+app = Syncopate()
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello, World!</h1>"
+
+
 if __name__ == "__main__":
-    app = Syncopate()
     syncopate.run(app)
