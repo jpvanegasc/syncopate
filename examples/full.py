3552ccef5cb485341727e42fb0d87e6ed8bf82c1
Juan Vanegas <juan.vanegas@loka.com>
2025-02-07 17:17:57 -0500

    :white_check_mark: Run sync and async tasks

--- examples/full.py
+++ examples/full.py
@@ -1,13 +1,24 @@
 import logging
+import time
 
 import syncopate
 from syncopate.framework import Syncopate
+from syncopate.framework.background import BackgroundTask, BackgroundTasks
 from syncopate.framework.responses import HTMLResponse
 
 logger = logging.getLogger(__name__)
 
 
 app = Syncopate()
+
+
+async def async_background_task():
+    logger.info("Async Background Task")
+
+
+def sync_background_task():
+    time.sleep(1)
+    logger.info("Sync Background Task")
 
 
 @app.route("/")
@@ -17,7 +28,10 @@ async def root(request):
     if request.scope["method"] == "POST":
         data = await request.json()
         logger.info(data)
-        return HTMLResponse("<h1>Hello, World!</h1>")
+        tasks = BackgroundTasks()
+        tasks.add_task(BackgroundTask(async_background_task))
+        tasks.add_task(BackgroundTask(sync_background_task))
+        return HTMLResponse("<h1>Hello, World!</h1>", background=tasks)
     return HTMLResponse("<h1>Method Not Allowed</h1>", status=405)
 
 
