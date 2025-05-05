a3c561ccfa9783ee9b104eae392f1a5e71dbf288
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2025-05-05 09:33:13 -0500

    :construction: Improve loop helpers

--- syncopate/loop/__init__.py
+++ syncopate/loop/__init__.py
@@ -1,6 +1,17 @@
 from syncopate.loop.events import EventLoop
 
+EVENT_LOOP = None
+
 
 def get_event_loop():
-    # TODO: implement a way to get the current event loop
-    return EventLoop()
+    global EVENT_LOOP
+    if EVENT_LOOP is None:
+        EVENT_LOOP = EventLoop()
+    return EVENT_LOOP
+
+
+def get_running_loop():
+    global EVENT_LOOP
+    if EVENT_LOOP is None:
+        raise RuntimeError("No running event loop")
+    return EVENT_LOOP
