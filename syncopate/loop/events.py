88d86c5ead4d06d22d3e2715dddaab6f69960f53
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2025-07-05 14:13:59 -0500

    :sparkles: Working call_soon

--- syncopate/loop/events.py
+++ syncopate/loop/events.py
@@ -1,1 +1,30 @@
-class EventLoop: ...
+from collections import deque
+
+
+class Handle:
+    def __init__(self, callback, args):
+        self._callback = callback
+        self._args = args
+
+    def _run(self):
+        self._callback(*self._args)
+
+
+class EventLoop:
+    def __init__(self):
+        self._ready = deque()
+        self._stopping = False
+
+    def run_forever(self):
+        while True:
+            self._run_once()
+            if self._stopping:
+                break
+
+    def call_soon(self, callback, *args):
+        self._ready.append(Handle(callback, args))
+
+    def _run_once(self):
+        while self._ready:
+            handle = self._ready.popleft()
+            handle._run()
--- tests/loop/test_events.py
+++ tests/loop/test_events.py
@@ -1,6 +1,22 @@
+from unittest.mock import Mock
+
+import pytest
+
 from syncopate.loop.events import EventLoop
 
 
-def test_event_loop():
-    loop = EventLoop()
-    assert loop
+@pytest.fixture
+def loop():
+    yield EventLoop()
+
+
+def test__run_once(loop):
+    mock_cb = Mock()
+    loop.call_soon(mock_cb, 1, 2, 3)
+
+    assert loop._ready
+
+    loop._run_once()
+
+    assert not loop._ready
+    mock_cb.assert_called_once_with(1, 2, 3)
