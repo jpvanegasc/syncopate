df8a3a4742e5675e141a81d78597576f87ddf344
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2025-07-25 10:55:28 -0500

    :sparkles: Add done callbacks to futures

--- syncopate/loop/futures.py
+++ syncopate/loop/futures.py
@@ -10,6 +10,7 @@ class Future:
 
     def __init__(self, *, loop):
         self._loop = loop
+        self._callbacks = []
 
     def result(self):
         if self._state != FINISHED:
@@ -19,3 +20,21 @@ class Future:
     def set_result(self, result):
         self._state = FINISHED
         self._result = result
+        self.__schedule_callbacks()
+
+    def add_done_callback(self, callback):
+        if not callable(callback):
+            raise TypeError("callback must be callable")
+        self._callbacks.append(callback)
+
+    def __schedule_callbacks(self):
+        callbacks = self._callbacks
+        if not callbacks:
+            return
+
+        self._callbacks = []
+        for callback in callbacks:
+            self._loop.call_soon(callback, self)
+
+    def __await__(self):
+        return (yield self)
