b32957e5bb924b8e432231afa395d713f668aaf3
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2025-07-25 14:25:57 -0500

    :white_check_mark: Add test cases

--- tests/app.py
+++ tests/app.py
@@ -2,7 +2,8 @@ import logging
 
 import syncopate
 from syncopate.framework import Syncopate
-from syncopate.framework.responses import HTMLResponse
+from syncopate.framework.requests import Request
+from syncopate.framework.responses import HTMLResponse, JSONResponse
 
 logger = logging.getLogger(__name__)
 
@@ -10,11 +11,18 @@ logger = logging.getLogger(__name__)
 app = Syncopate()
 
 
-@app.route("/get/sync")
-async def root(request):
+@app.route("/async/get")
+async def async_get(request: Request):
     if request.scope["method"] != "GET":
         return HTMLResponse("<h1>Method Not Allowed</h1>", status=405)
-    return HTMLResponse("/get/sync")
+    return HTMLResponse("/async/get")
+
+
+@app.route("/async/post")
+async def async_post(request: Request):
+    if request.scope["method"] != "POST":
+        return HTMLResponse("<h1>Method Not Allowed</h1>", status=405)
+    return JSONResponse(await request.json())
 
 
 if __name__ == "__main__":
