1c33589b02cc960b492f9e7ff6d14c86953182fb
Juan Pablo Vanegas <jpvanegasc@gmail.com>
2025-08-26 19:09:13 -0500

    :white_check_mark: Add background task test cases

--- tests/app.py
+++ tests/app.py
@@ -1,7 +1,9 @@
 import logging
+import time
 
 import syncopate
 from syncopate.framework import Syncopate
+from syncopate.framework.background import BackgroundTask, BackgroundTasks
 from syncopate.framework.requests import Request
 from syncopate.framework.responses import HTMLResponse, JSONResponse
 
@@ -9,6 +11,15 @@ logger = logging.getLogger(__name__)
 
 
 app = Syncopate()
+
+
+async def async_background_task():
+    logger.info("Async Background Task")
+
+
+def sync_background_task():
+    time.sleep(1)
+    logger.info("Sync Background Task")
 
 
 @app.route("/async/get")
@@ -22,7 +33,10 @@ async def async_get(request: Request):
 async def async_post(request: Request):
     if request.scope["method"] != "POST":
         return HTMLResponse("<h1>Method Not Allowed</h1>", status=405)
-    return JSONResponse(await request.json())
+    tasks = BackgroundTasks()
+    tasks.add_task(BackgroundTask(async_background_task))
+    tasks.add_task(BackgroundTask(sync_background_task))
+    return JSONResponse(await request.json(), background=tasks)
 
 
 if __name__ == "__main__":
